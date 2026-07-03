dee542ff5681e333b9dcd9cecbcc90a88bc8556e
sarafidi <sarafina.rafidi@student.mmu.edu.my>
2026-07-03 16:24:06 +0800

    [create] Create IDGenerator util class

---
+++ src/main/java/util/IDGenerator.java
@@ -0,0 +1,63 @@
+package util;
+
+import model.Bill;
+import model.Rental;
+import model.equipment.Equipment;
+import model.user.User;
+
+import java.util.ArrayList;
+
+public class IDGenerator {
+    private static int extractMaxNumeric(ArrayList<String> ids) {
+        // id.split("-") gives ["PAT", "0023"]
+        // [1] gives "0023"
+        // parseInt converts to 23
+        int max = 0;
+        for (String id : ids) {
+            if (id == null || !id.contains("-")) continue;
+            try {
+                int num = Integer.parseInt(id.split("-")[1]);
+                if (num > max) max = num;
+            } catch (NumberFormatException | ArrayIndexOutOfBoundsException e) {
+                // ignore malformed IDs
+            }
+        }
+        return max;
+    }
+
+    public static String generateUserId() {
+        ArrayList<String> ids = new ArrayList<>();
+        for (User p : DataStore.getInstance().getUser()) {
+            ids.add(p.getUserId());
+        }
+        // %04d -> integer, min 4 digits, pad with zeros on left
+        return String.format("USR-%04d", extractMaxNumeric(ids) + 1);
+    }
+
+    public static String generateEquipmentId() {
+        ArrayList<String> ids = new ArrayList<>();
+        for (Equipment p : DataStore.getInstance().getEquipment()) {
+            ids.add(p.getEquipmentId());
+        }
+        // %04d -> integer, min 4 digits, pad with zeros on left
+        return String.format("EQ-%04d", extractMaxNumeric(ids) + 1);
+    }
+
+    public static String generateRentalId() {
+        ArrayList<String> ids = new ArrayList<>();
+        for (Rental p : DataStore.getInstance().getRental()) {
+            ids.add(p.getRentalId());
+        }
+        // %04d -> integer, min 4 digits, pad with zeros on left
+        return String.format("RN-%04d", extractMaxNumeric(ids) + 1);
+    }
+
+    public static String generateBillId() {
+        ArrayList<String> ids = new ArrayList<>();
+        for (Bill p : DataStore.getInstance().getBill()) {
+            ids.add(p.getBillId());
+        }
+        // %04d -> integer, min 4 digits, pad with zeros on left
+        return String.format("BL-%04d", extractMaxNumeric(ids) + 1);
+    }
+}
