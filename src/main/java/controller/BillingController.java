9f55640df8af2ed21afc2c1b253eab2ce2f3741d
Putra <aimanzarif.putra.hairol@student.mmu.edu.my>
2026-07-06 14:50:51 +0800

    Add files via upload

--- src/main/java/controller/BillingController.java
+++ src/main/java/controller/BillingController.java
@@ -2,28 +2,41 @@ package controller;
 
 import model.RentalSystemFacade;
 import model.bill.Bill;
+import util.Validator;
 
+import java.util.Collections;
 import java.util.List;
 
 public class BillingController {
     private final RentalSystemFacade facade = RentalSystemFacade.getInstance();
 
     public Bill generateBill(String rentalId) {
-        // TODO: TO BE IMPLEMENTED BY MEMBER B
-        // FIX: return type calls facade billing methods
-        return null;
+        if (!Validator.isNonEmpty(rentalId)) {
+            throw new IllegalArgumentException("Please enter a Rental ID.");
+        }
+
+        Bill existing = facade.findBillByRental(rentalId);
+        if (existing != null) {
+            return existing;
+        }
+
+        // facade throws IllegalArgumentException if the rental ID doesn't exist -
+        // let it propagate up so the panel can show the error to the user.
+        return facade.generateBill(rentalId);
     }
 
     public Bill findBillByRental(String rentalId) {
-        // TODO: TO BE IMPLEMENTED BY MEMBER B
-        // FIX: return type calls facade billing methods
-        return null;
+        if (!Validator.isNonEmpty(rentalId)) {
+            return null;
+        }
+        return facade.findBillByRental(rentalId);
     }
 
     public List<Bill> getBillHistory(String userId) {
-        // TODO: TO BE IMPLEMENTED BY MEMBER B
-        // FIX: return type calls facade billing methods
-        return null;
+        if (!Validator.isNonEmpty(userId)) {
+            return Collections.emptyList();
+        }
+        return facade.getBillHistory(userId);
     }
 
 }
