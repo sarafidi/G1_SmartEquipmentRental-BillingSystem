c733d92b77520801cbead4dd7378f03aff8559a9
sarafidi <sarafina.rafidi@student.mmu.edu.my>
2026-07-08 11:16:27 +0800

    [checklist #2] Solve Billing

--- src/main/java/controller/BillingController.java
+++ src/main/java/controller/BillingController.java
@@ -4,13 +4,27 @@ import model.RentalSystemFacade;
 import model.bill.Bill;
 import model.rental.Rental;
 import model.user.User;
+import model.RentalStatus;
+import model.UserType;
+import model.strategy.LatePenalty;
+import model.strategy.DamagePenalty;
 import util.Validator;
 
+import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
 import java.util.Collections;
 import java.util.List;
 
 public class BillingController {
     private final RentalSystemFacade facade = RentalSystemFacade.getInstance();
+
+    public double getLateFee(Rental rental) {
+        return new LatePenalty(10.00).computePenalty(rental);
+    }
+
+    public double getDamageFee(Rental rental) {
+        return new DamagePenalty().computePenalty(rental);
+    }
 
     public Bill generateBill(String rentalId) {
         if (!Validator.isNonEmpty(rentalId)) {
@@ -19,8 +33,8 @@ public class BillingController {
         
         // Security Check: Students can only view/generate bills for their own rentals
         User loggedInUser = facade.getCurrentUser();
-        if (loggedInUser.getUserType() != model.UserType.ADMIN && loggedInUser.getUserType() != model.UserType.STAFF) {
-            model.rental.Rental rental = facade.findRentalById(rentalId);
+        if (loggedInUser.getUserType() != UserType.ADMIN && loggedInUser.getUserType() != UserType.STAFF) {
+            Rental rental = facade.findRentalById(rentalId);
             if (rental != null && !rental.getUser().getUserId().equalsIgnoreCase(loggedInUser.getUserId())) {
                 throw new IllegalArgumentException("Access Denied: You can only view bills for your own rentals.");
             }
@@ -36,8 +50,8 @@ public class BillingController {
         
         // Security Check: Students can only search for their own bills
         User loggedInUser = facade.getCurrentUser();
-        if (loggedInUser.getUserType() != model.UserType.ADMIN && loggedInUser.getUserType() != model.UserType.STAFF) {
-            model.rental.Rental rental = facade.findRentalById(rentalId);
+        if (loggedInUser.getUserType() != UserType.ADMIN && loggedInUser.getUserType() != UserType.STAFF) {
+            Rental rental = facade.findRentalById(rentalId);
             if (rental != null && !rental.getUser().getUserId().equalsIgnoreCase(loggedInUser.getUserId())) {
                 return null;
             }
